b4427df45dfc056c67eff7525b9d5d1f599da811
yangyuxingithub <yyx18838519738@163.com>
2020-07-29 10:49:23 +0800

    提交用户以及菜单实体代码

--- goSystemManage/goAdmin/model/SysRoleMenu.go
+++ goSystemManage/goAdmin/model/SysRoleMenu.go
@@ -4,4 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type SysRoleMenu struct {
 	gorm.Model
+	MenuId   int           `json:"menuId" gorm:"comment:'菜单id'"`
+	RoleId   int           `json:"roleId" gorm:"comment:'角色id'"`
+	Children []SysRoleMenu `json:"children"`
 }
